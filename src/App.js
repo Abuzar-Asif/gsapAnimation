abc1648e9c6a6409d15b4bc507d0363027f2bd7f
Abuzar-asif <Abuzar@pixarsart.com>
2025-02-05 17:36:34 +0500

    created the react folder for gsap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <center>
+      <h1>
+        GSAP Animation practice 
+      </h1>
+    </center>
+    </>
+  )
+}
+
+export default App
